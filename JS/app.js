e9a3e7598be55123d8d0c080ad842aef5d983112
Tomas Perez Diaz <tomasperezdiaz@gmail.com>
2023-12-13 13:09:39 -0300

    Calculadora mejorada

--- JS/app.js
+++ JS/app.js
@@ -4,27 +4,21 @@ const botones = document.querySelectorAll(".boton");
 botones.forEach((boton) => {
   boton.addEventListener("click", () => {
     const botonApretado = boton.textContent;
+
     if (boton.id === "c") {
       pantalla.textContent = "0";
-      return;
     } else if (boton.id === "borrar") {
-      if (
-        pantalla.textContent.length === 1 ||
-        pantalla.textContent === "Error"
-      ) {
-        pantalla.textContent = "0";
-      } else {
-        pantalla.textContent = pantalla.textContent.slice(0, -1);
-      }
-      return;
+      pantalla.textContent = pantalla.textContent.length === 1 || pantalla.textContent === "Error"
+        ? "0"
+        : pantalla.textContent.slice(0, -1);
     } else if (boton.id === "igual") {
       try {
-        pantalla.textContent = eval(pantalla.textContent);
-      } catch {
+        const resultado = eval(pantalla.textContent);
+        pantalla.textContent = Number.isFinite(resultado) ? resultado : "Error";
+      } catch (error) {
         pantalla.textContent = "Error";
       }
-    } else if (pantalla.textContent === "0" ||
-    pantalla.textContent === "Error" ) {
+    } else if (pantalla.textContent === "0" || pantalla.textContent === "Error") {
       pantalla.textContent = botonApretado;
     } else {
       pantalla.textContent += botonApretado;
